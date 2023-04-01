650175559329597984d537f3de84cfeaf8f561f0
Andrea Stocco <astocco11@gmail.com>
2023-04-01 11:09:52 +0200

    remove catkin_ws from repo

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/gino/gits/ros/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/gino/gits/ros/devel;/opt/ros/noetic'.split(';') if '/home/gino/gits/ros/devel;/opt/ros/noetic' != '' else []
